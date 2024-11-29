a005c25f2196dc7947c86e87f36d309a856512b9
Viniciusovski <vini98.2012@gmail.com>
2024-11-29 16:44:50 -0300

    Utilizando a API de tradução MyMemory

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,9 +1,6 @@
 package br.com.alura.screenmatch.model;
 
-import br.com.alura.screenmatch.service.ConsultaChatGPT;
-import com.fasterxml.jackson.annotation.JsonAlias;
-
-import java.util.Optional;
+import br.com.alura.screenmatch.service.traducao.ConsultaMyMemory;
 import java.util.OptionalDouble;
 
 public class Serie {
@@ -22,7 +19,7 @@ public class Serie {
         this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
-        this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
+        this.sinopse = ConsultaMyMemory.obterTraducao(dadosSerie.sinopse()).trim();
     }
 
     public String getTitulo() {
